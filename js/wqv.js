bdfeda8020d31df49b619047e8844e67a62146cb
Project-S-31415 <projects31415.gif@gmail.com>
2023-02-22 18:22:37 +0900

    web版wqvの準備

---
+++ js/wqv.js
@@ -0,0 +1,28 @@
+window.onload = function()
+{
+  getJson();//作成時はコメントアウトする
+  //setTimeout("getJson()", 10000);
+  //setInterval("getJson()", 30000);
+}
+
+function getJson() 
+{
+  let request = new XMLHttpRequest();
+  request.open("GET", "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson");
+  request.send();
+  request.onreadystatechange = () => 
+  {
+    if (request.readyState == 4 && request.status == 200) 
+    {
+      let json = JSON.parse(request.responseText);
+      for (let i = 0; i < 1; i++) //json.features.length
+      {
+        let text = "震源:"+json.features[i].properties.place+"<br>"
+        +json.features[i].geometry.coordinates[1]+"°,"+json.features[i].geometry.coordinates[0]+"°<br>深さ:"
+        +json.features[i].geometry.coordinates[2]+"km<br>"
+        +json.features[i].properties.magType+json.features[i].properties.mag + "  改正メルカリ震度階級:"+json.features[i].properties.mmi;
+        document.getElementById("main").innerHTML = text;
+      }
+    }
+  }
+}
